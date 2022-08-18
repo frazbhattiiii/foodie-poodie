a1bc317217a25df3c85bb3de8b973ba3f09ccb31
Muhammad Ahmed Fraz <bhattifraz94@gmail.com>
2022-08-19 02:14:35 +0500

    mobile nav and logout

--- src/shared/components/Header.jsx
+++ src/shared/components/Header.jsx
@@ -27,10 +27,21 @@ function Header ( props ) {
             setIsMenu(!IsMenu)
         }
     }
+    const logout = async () => {
+        const done =  window.confirm('Are you sure you want to logout?')
+        if(done){
+            setIsMenu ( false );
+            localStorage.clear ();
+            dispatch ( {
+                           type : actionType.SET_USER , user : null
+                       } )
+            await firebaseAuth.signOut ();
+        }
+    }
 
 
     return (
-        <div className='fixed z-50 w-screen p-6 px-16'>
+        <div className='fixed z-50 w-screen p-3 px-8 md:p-6 md:px-16'>
             {/*For Larger Screen*/ }
             <div className='hidden md:flex w-full h-full flex items-center justify-between'>
                 <Link to={ '/' } className='flex items-center gap-2'>
@@ -98,7 +109,60 @@ function Header ( props ) {
                 </div>
             </div>
             {/*    For Mobile*/ }
-            <div className='flex md:hidden w-full h-full'>
+            <div className='flex items-center justify-between md:hidden w-full h-full'>
+                <Link to={ '/' } className='flex items-center gap-2'>
+                    <img src={ logo } alt='logo' className='w-10 object-cover'/>
+                    <p className='text-xl font-bold text-headingColor'>Foodie Poodie</p>
+                </Link>
+                <div className='relative '>
+                    <motion.img
+                        whileTap={ { scale : 0.6 } }
+                        src={ user ? user.photoURL : Avatar } alt='avatar'
+                        className='w-10 min-w-[40]px h-10 min-h-[40]px drop-shadow-2xl cursor-pointer rounded-full'
+                        onClick={ login }
+                    />
+                    {
+                        IsMenu  && (
+                            <motion.div
+                                initial={{opacity:0, scale :0.6}}
+                                animate={{opacity:5, scale :1}}
+                                exit={{opacity:0, scale :0.6}}
+                                className='w-40 bg-gray-50 shadow-xl rounded-lg flex flex-col absolute top-12 right-0'>
+                                { user && user.email === 'bhattifraz94@gmail.com' && (
+                                    <Link to={ '/createItem' }>
+                                        <p className='px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base'>
+                                            New Item <MdAdd/>
+                                        </p>
+                                    </Link>
+                                ) }
+                                <ul
+                                    className='flex flex-col'>
+                                    <li className='text-base text-textColor hover:text-headingColor duration-100 hover:bg-slate-100
+                    transition-all ease-in-out cursor-pointer px-4 py-2'>
+                                        Home
+                                    </li>
+                                    <li className='text-base text-textColor hover:text-headingColor hover:bg-slate-100 duration-100
+                    transition-all ease-in-out cursor-pointer px-4 py-2'>
+                                        Menu
+                                    </li>
+                                    <li className='text-base text-textColor hover:text-headingColor duration-100 hover:bg-slate-100
+                    transition-all ease-in-out cursor-pointer px-4 py-2'>
+                                        About Us
+                                    </li>
+                                    <li className='text-base text-textColor hover:text-headingColor  duration-100 hover:bg-slate-100
+                    transition-all ease-in-out cursor-pointer px-4 py-2'>
+                                        Contact Us
+                                    </li>
+                                </ul>
+                                <p className='m-2 p-2 px-4 py-2 flex items-center gap-3 cursor-pointer  hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base bg-gray-200 justify-center hover:bg-gray-300'
+                                onClick={logout}
+                                >
+                                    logout <MdLogout/>
+                                </p>
+                            </motion.div>
+                        )
+                    }
+                </div>
 
             </div>
         </div>
