8c86be9d3fafbfcefecaed32b22e3ecc4498a56c
Muhammad Ahmed Fraz <bhattifraz94@gmail.com>
2022-08-18 06:50:58 +0500

    setting tailwind

--- src/App.js
+++ src/App.js
@@ -2,11 +2,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>
-        Hello world
+      <h1 className="text-3xl font-bold underline">
+          Hello world!
       </h1>
-  </div>
   );
 }
 
