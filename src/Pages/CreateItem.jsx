570a923f4d2a747edc5097871d911b6d1c3d4272
Muhammad Ahmed Fraz <bhattifraz94@gmail.com>
2022-08-23 18:41:32 +0500

    adding categories data

--- src/Pages/CreateItem.jsx
+++ src/Pages/CreateItem.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React           from 'react';
+import CreateContainer from "../shared/components/CreateContainer";
 
 function CreateItem ( props ) {
     return (
-        <div>
-            Item is going to create here!
-        </div>
+        <>
+            <CreateContainer/>
+        </>
     );
 }
 
